61240cb8a10222efaf46260d34b01ffbe21709a3
Eshita Agarwal <eshitaagarwal55@gmail.com>
2022-09-26 16:59:50 -0400

    Add constants

---
+++ src/main/java/org/game/connect4/util/GameConstants.java
@@ -0,0 +1,8 @@
+package org.game.connect4.util;
+
+public class GameConstants {
+    public static final String EMPTY_SLOT = ".";
+    public static final int DEFAULT_HEIGHT = 6;
+    public static final int DEFAULT_WIDTH = 7;
+
+}
